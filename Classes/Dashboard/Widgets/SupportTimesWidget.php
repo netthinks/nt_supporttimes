78832b9c943820f6435d19675ee2f0dd99abf777
Dietmar Engler <info@netthinks.com>
2025-12-29 17:04:34 +0100

    refactor: Instantiate StandaloneView directly within the widget content rendering method instead of injecting it.

--- Classes/Dashboard/Widgets/SupportTimesWidget.php
+++ Classes/Dashboard/Widgets/SupportTimesWidget.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Netthinks\NtSupporttimes\Dashboard\Widgets;
 
 use Netthinks\NtSupporttimes\Service\ReleaseService;
-use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Dashboard\Widgets\WidgetConfigurationInterface;
 use TYPO3\CMS\Dashboard\Widgets\WidgetInterface;
 use TYPO3\CMS\Fluid\View\StandaloneView;
@@ -15,22 +15,23 @@ class SupportTimesWidget implements WidgetInterface
     public function __construct(
         private readonly WidgetConfigurationInterface $configuration,
         private readonly ReleaseService $releaseService,
-        private readonly StandaloneView $view,
-        private readonly PageRenderer $pageRenderer,
         private readonly array $options = []
     ) {
     }
 
     public function renderWidgetContent(): string
     {
-        $this->view->setTemplatePathAndFilename('EXT:nt_supporttimes/Resources/Private/Templates/Dashboard/SupportTimes.html');
-        $this->view->assignMultiple([
+        $view = GeneralUtility::makeInstance(StandaloneView::class);
+        $view->setTemplatePathAndFilename(
+            GeneralUtility::getFileAbsFileName('EXT:nt_supporttimes/Resources/Private/Templates/Dashboard/SupportTimes.html')
+        );
+        $view->assignMultiple([
             'releaseData' => $this->releaseService->getReleaseData(),
             'options' => $this->options,
             'configuration' => $this->configuration,
         ]);
 
-        return $this->view->render();
+        return $view->render();
     }
 
     public function getOptions(): array
