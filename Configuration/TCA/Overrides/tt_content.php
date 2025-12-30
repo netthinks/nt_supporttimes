4e48d387c1403d838e4c983346ec0be98f87d5cf
Dietmar Engler <info@netthinks.com>
2025-12-30 08:40:19 +0100

    feat: Add plugin description to language files and refactor `tt_content` registration to directly include FlexForm and description for TYPO3 v14+.

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -5,37 +5,38 @@ use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'nt_supporttimes',
-    'Pi1',
-    'LLL:EXT:nt_supporttimes/Resources/Private/Language/locallang_db.xlf:plugin.pi1.title',
-    'ext-ntsupporttimes-icon'
-);
-
-$pluginSignature = 'ntsupporttimes_pi1';
-
-// Get TYPO3 version
 $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
 $version = $versionInformation->getMajorVersion();
 
-// Add FlexForm
-ExtensionManagementUtility::addPiFlexFormValue(
-    $pluginSignature,
-    'FILE:EXT:nt_supporttimes/Configuration/FlexForms/Pi1.xml'
-);
-
 if ($version >= 14) {
-    // TYPO3 14: Use addToAllTCAtypes to explicitly add pi_flexform to the plugin tab
-    ExtensionManagementUtility::addToAllTCAtypes(
-        'tt_content',
-        'pi_flexform',
-        $pluginSignature,
-        'after:subheader'
+    // TYPO3 14: Pass FlexForm directly to registerPlugin
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+        'nt_supporttimes',
+        'Pi1',
+        'LLL:EXT:nt_supporttimes/Resources/Private/Language/locallang_db.xlf:plugin.pi1.title',
+        'ext-ntsupporttimes-icon',
+        'plugins',
+        'LLL:EXT:nt_supporttimes/Resources/Private/Language/locallang_db.xlf:plugin.pi1.description',
+        'FILE:EXT:nt_supporttimes/Configuration/FlexForms/Pi1.xml'
     );
 } else {
-    // TYPO3 12 & 13: Standard configuration
+    // TYPO3 12 & 13: Old method without FlexForm parameter
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+        'nt_supporttimes',
+        'Pi1',
+        'LLL:EXT:nt_supporttimes/Resources/Private/Language/locallang_db.xlf:plugin.pi1.title',
+        'ext-ntsupporttimes-icon'
+    );
+    
+    $pluginSignature = 'ntsupporttimes_pi1';
+    
+    // Add FlexForm the old way
+    ExtensionManagementUtility::addPiFlexFormValue(
+        $pluginSignature,
+        'FILE:EXT:nt_supporttimes/Configuration/FlexForms/Pi1.xml'
+    );
+    
+    // Standard configuration
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'pages,recursive,select_key';
 }
-
-// Exclude standard fields for all versions
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'pages,recursive,select_key';
