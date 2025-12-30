4a4b9e1bd22c7dff5b7f1d665016156e99a9eaaf
Dietmar Engler <info@netthinks.com>
2025-12-30 08:26:56 +0100

    refactor: Update FlexForm registration to direct TCA configuration for TYPO3 v12+

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -10,13 +10,9 @@ defined('TYPO3') or die();
 
 $pluginSignature = 'ntsupporttimes_pi1';
 
-// Add FlexForm
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    '*',
-    'FILE:EXT:nt_supporttimes/Configuration/FlexForms/Pi1.xml',
-    $pluginSignature
-);
-
-// Configure the plugin to show pi_flexform
+// Add FlexForm configuration
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'pages,recursive,select_key';
+
+// Configure pi_flexform field for this plugin
+$GLOBALS['TCA']['tt_content']['columns']['pi_flexform']['config']['ds'][$pluginSignature] = 'FILE:EXT:nt_supporttimes/Configuration/FlexForms/Pi1.xml';
