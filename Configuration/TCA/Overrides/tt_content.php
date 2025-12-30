b7ae2eedf39cd0ce5c8975907f4be645895540a2
Dietmar Engler <info@netthinks.com>
2025-12-30 08:33:45 +0100

    refactor: Adapt TCA configuration for plugin FlexForm to use `addToAllTCAtypes` for TYPO3 v14 and `subtypes_addlist` for v12/13.

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,6 +3,7 @@ defined('TYPO3') or die();
 
 use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'nt_supporttimes',
@@ -18,17 +19,23 @@ $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
 $version = $versionInformation->getMajorVersion();
 
 // Add FlexForm
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+ExtensionManagementUtility::addPiFlexFormValue(
     $pluginSignature,
     'FILE:EXT:nt_supporttimes/Configuration/FlexForms/Pi1.xml'
 );
 
-// Standard configuration for TYPO3 12 and 13
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'pages,recursive,select_key';
-
-// TYPO3 14 specific: Ensure FlexForm is in columnsOverrides
 if ($version >= 14) {
-    $GLOBALS['TCA']['tt_content']['types']['list']['columnsOverrides']['pi_flexform']['config']['ds'][$pluginSignature] = 
-        'FILE:EXT:nt_supporttimes/Configuration/FlexForms/Pi1.xml';
+    // TYPO3 14: Use addToAllTCAtypes to explicitly add pi_flexform to the plugin tab
+    ExtensionManagementUtility::addToAllTCAtypes(
+        'tt_content',
+        'pi_flexform',
+        $pluginSignature,
+        'after:subheader'
+    );
+} else {
+    // TYPO3 12 & 13: Standard configuration
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 }
+
+// Exclude standard fields for all versions
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'pages,recursive,select_key';
