89ebb8801ea071d8a3e927a12b10ff7352420d58
Dietmar Engler <info@netthinks.com>
2025-12-30 08:31:31 +0100

    feat: Add TYPO3 v14 compatibility for FlexForm TCA configuration.

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,6 +1,9 @@
 <?php
 defined('TYPO3') or die();
 
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'nt_supporttimes',
     'Pi1',
@@ -10,14 +13,22 @@ defined('TYPO3') or die();
 
 $pluginSignature = 'ntsupporttimes_pi1';
 
+// Get TYPO3 version
+$versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+$version = $versionInformation->getMajorVersion();
+
 // Add FlexForm
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
     $pluginSignature,
     'FILE:EXT:nt_supporttimes/Configuration/FlexForms/Pi1.xml'
 );
 
-// Add pi_flexform to showitem for the plugin
+// Standard configuration for TYPO3 12 and 13
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-
-// Exclude standard fields
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'pages,recursive,select_key';
+
+// TYPO3 14 specific: Ensure FlexForm is in columnsOverrides
+if ($version >= 14) {
+    $GLOBALS['TCA']['tt_content']['types']['list']['columnsOverrides']['pi_flexform']['config']['ds'][$pluginSignature] = 
+        'FILE:EXT:nt_supporttimes/Configuration/FlexForms/Pi1.xml';
+}
